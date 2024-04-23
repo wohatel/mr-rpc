379f4a1dd9864b2b8359260f9664a099a66f7333
yaochuang <chuang.yao1081@seaboxdata.com>
2024-04-23 19:04:36 +0800

    添加注释

--- src/main/java/com/murong/rpc/interact/MrServerAroundAdvice.java
+++ src/main/java/com/murong/rpc/interact/MrServerAroundAdvice.java
@@ -9,5 +9,14 @@ import java.lang.reflect.InvocationTargetException;
  * @author yaochuang 2024/04/22 12:52
  */
 public interface MrServerAroundAdvice {
+
+    /**
+     * 切面通知
+     *
+     * @param joinPoint 切入点
+     * @return object 响应结果
+     * @throws InvocationTargetException 执行异常
+     * @throws IllegalAccessException    参数异常
+     */
     Object proceed(ProceedJoinPoint joinPoint) throws InvocationTargetException, IllegalAccessException;
 }
